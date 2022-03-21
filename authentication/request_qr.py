d66c2aaa67b30eb4a7fc93f5e2050fdf200f29d0
tnick1502 <59287059+tnick1502@users.noreply.github.com>
2022-03-21 14:19:21 +0300

    создан модуль запроса на сервер для генерайии qr кода

---
+++ authentication/request_qr.py
@@ -0,0 +1,28 @@
+import requests
+HOST = "http://192.168.0.76:8000/"
+
+new_show = {
+        "labolatory": "mdgt",
+        "password": "it_user",
+
+        "test_name": "triaxial",
+        "object": "233445",
+        "labolatory_number": 12,
+        "test_type": "mdgt",
+
+        "data": {
+            "Лабораторный номер": "123-ab",
+            "Модуль деформации E50, МПа": "15.8",
+            "Девиатор разрушения qf, МПа": "1.02"
+        }
+    }
+
+def request_qr(data):
+    response = requests.post('{HOST}report/', json=data)
+    assert response.ok, "Не удалось сгенерировать код"
+    with open("qr.png", "wb") as file:
+        file.write(response.content)
+    return "qr.png"
+
+if __name__=="__main__":
+    print(request_qr(new_show))
--- test.py
+++ test.py
@@ -1,1 +1,26 @@
-assert False, "ogb"
+import requests
+import json
+
+
+new_show = {
+    "labolatory": "mdgt",
+    "password": "it_user",
+
+    "test_name": "triaxial",
+    "object": "233445",
+    "labolatory_number": 12,
+    "test_type": "mdgt",
+
+    "data": {
+        "Лабораторный номер": "123-ab",
+        "Модуль деформации E50, МПа": "15.8",
+        "Девиатор разрушения qf, МПа": "1.02"
+    }
+}
+response = requests.post('http://192.168.0.76:8000/report/', json=new_show)
+if response.ok:
+    print("Upload completed successfully!")
+    with open("D:/Торренты/Курсы молчанов/Flask/qr.png", "wb") as file:
+        file.write(response.content)
+else:
+    print("Something went wrong!")
