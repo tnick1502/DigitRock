c43301d0daa83ce3266e0b5c23c0124d822c2949
tnick1502 <59287059+tnick1502@users.noreply.github.com>
2022-04-01 17:00:41 +0300

    Добавил виджет физики к вибро

--- vibration_creep/vibration_creep_widgets.py
+++ vibration_creep/vibration_creep_widgets.py
@@ -9,6 +9,7 @@ import shutil
 import os
 import threading
 
+from excel_statment.initial_tables import LinePhysicalProperties
 from excel_statment.initial_statment_widgets import VibrationCreepStatment
 from general.reports import report_VibrationCreep, report_VibrationCreep3, zap
 from general.save_widget import Save_Dir
@@ -34,8 +35,9 @@ class VibrationCreepSoilTestWidget(QWidget):
         self._create_Ui()
 
     def _create_Ui(self):
+        self.main_layout = QVBoxLayout(self)
 
-        self.layout = QHBoxLayout(self)
+        self.layout = QHBoxLayout()
         self.dynamic_widget = VibrationCreepUI()
         self.layout_2 = QVBoxLayout()
 
@@ -62,14 +64,11 @@ class VibrationCreepSoilTestWidget(QWidget):
         self.identification.setFixedHeight(700)
         self.layout.addWidget(self.dynamic_widget)
 
-        self.refresh_button = QPushButton("Обновить")
-        self.refresh_button.clicked.connect(self._refresh)
-
         self.layout_2.addWidget(self.identification)
         self.layout.addLayout(self.layout_2)
-        self.layout_2.addWidget(self.refresh_button)
         self.layout_2.addStretch(-1)
-        self.layout.setContentsMargins(5, 5, 5, 5)
+        self.main_layout.setContentsMargins(5, 5, 5, 5)
+        self.main_layout.addLayout(self.layout)
 
     def set_test_params(self, params):
         """Полкчение параметров образца и передача в классы модели и ползунков"""
@@ -304,11 +303,24 @@ class VibrationCreepSoilTestApp(QWidget):
 
         self.tab_4.general_statment_button.clicked.connect(self.general_statment)
 
+        self.physical_line_1 = LinePhysicalProperties()
+        self.tab_2.line_for_phiz.addWidget(self.physical_line_1)
+        self.tab_2.line_for_phiz.addStretch(-1)
+        self.physical_line_1.refresh_button.clicked.connect(self.tab_2.refresh)
+        self.physical_line_1.save_button.clicked.connect(self.save_report_and_continue)
+
+        self.physical_line_2 = LinePhysicalProperties()
+        self.tab_3.main_layout.insertWidget(0, self.physical_line_2)
+        self.physical_line_2.refresh_button.clicked.connect(self.tab_3._refresh)
+        self.physical_line_2.save_button.clicked.connect(self.save_report_and_continue)
+
     def _set_params(self, param):
         self.tab_2.set_params(param)
         self.tab_3.set_test_params(param)
         self.tab_3.identification.set_data()
         self.tab_2.item_identification.set_data()
+        self.physical_line_1.set_data()
+        self.physical_line_2.set_data()
 
     def save_report(self):
         try:
@@ -489,6 +501,23 @@ class VibrationCreepSoilTestApp(QWidget):
 
         _statment = StatementGenerator(self, path=s, statement_structure_key="triaxial_cyclic")
         _statment.show()
+
+    def save_report_and_continue(self):
+        try:
+            self.save_report()
+        except:
+            pass
+        keys = [key for key in statment]
+        for i, val in enumerate(keys):
+            if (val == statment.current_test) and (i < len(keys) - 1):
+                statment.current_test = keys[i+1]
+                self._set_params(True)
+                self.physical_line_1.set_data()
+                self.physical_line_2.set_data()
+                break
+            else:
+                pass
+
 
 
 
