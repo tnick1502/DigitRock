ce77e6993acbb4829a0b5b796e76b44c328f0a54
tnick1502 <tnick1502@mail.ru>
2023-05-03 17:54:55 +0300

    -

--- metrics/create.sql
+++ metrics/create.sql
@@ -37,3 +37,38 @@ ROUND (CAST (AVG (EXTRACT (EPOCH FROM (session_end - session_start))) AS numeric
 ROUND (CAST (AVG ( EXTRACT(EPOCH FROM (session_end - session_start)) / report_count) AS numeric), 1) AS avg_report_time
 FROM sessions
 GROUP BY test_type
+
+
+CREATE OR REPLACE FUNCTION get_avg_by_user (current_username varchar, current_year int, current_month int)
+RETURNS TABLE (test_type varchar, avg_session_duration numeric, avg_report_time numeric) AS $$
+	SELECT test_type,
+	ROUND (CAST (AVG (EXTRACT (EPOCH FROM (session_end - session_start))) AS numeric), 1) AS avg_duration,
+	ROUND (CAST (AVG ( EXTRACT(EPOCH FROM (session_end - session_start)) / report_count) AS numeric), 1) AS avg_report_time
+	FROM sessions
+	LEFT JOIN users USING (user_ip)
+	WHERE username = current_username AND EXTRACT(MONTH FROM session_start) = current_month AND EXTRACT(YEAR FROM session_start) = current_year
+	GROUP BY test_type
+$$ LANGUAGE SQL;
+
+CREATE OR REPLACE FUNCTION get_avg_by_test_type (current_year int, current_month int)
+RETURNS TABLE (test_type varchar, avg_session_duration numeric, avg_report_time numeric) AS $$
+	SELECT test_type,
+	ROUND (CAST (AVG (EXTRACT (EPOCH FROM (session_end - session_start))) AS numeric), 1) AS avg_duration,
+	ROUND (CAST (AVG ( EXTRACT(EPOCH FROM (session_end - session_start)) / report_count) AS numeric), 1) AS avg_report_time
+	FROM sessions
+	LEFT JOIN users USING (user_ip)
+	WHERE EXTRACT(MONTH FROM session_start) = current_month AND EXTRACT(YEAR FROM session_start) = current_year
+	GROUP BY test_type
+$$ LANGUAGE SQL;
+
+CREATE OR REPLACE FUNCTION get_sessions_by_user (current_username varchar, current_year int, current_month int)
+RETURNS TABLE (test_type varchar, session_start timestamp, session_duration numeric, report_count int, object_number varchar, avg_report_time numeric) AS $$
+	SELECT test_type, session_start,
+	ROUND (CAST ( EXTRACT(EPOCH FROM (session_end - session_start)) AS numeric), 1) AS session_duration,
+	report_count,
+	object_number,
+	ROUND (CAST ( EXTRACT(EPOCH FROM (session_end - session_start)) / report_count AS numeric), 1) AS avg_report_time
+	FROM sessions
+	LEFT JOIN users USING (user_ip)
+	WHERE username = current_username AND EXTRACT(MONTH FROM session_start) = current_month AND EXTRACT(YEAR FROM session_start) = current_year
+$$ LANGUAGE SQL;
